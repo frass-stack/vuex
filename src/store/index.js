95111fba96743e467be1909c870a2751f7412e21
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-21 14:38:28 -0300

    Modules: Se implementa el concepto de modulo.

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,9 @@
 import { createStore } from 'vuex'
-import getRandomInt from '@/helpers/getRandomInt'
+import counterStore from './counter'
+
 
 export default createStore({
-    state: {
-        count: 1,
-        lastMutation: 'none',
-        isLoading: false,
-        lastRandomInt: null
-    },
-    mutations:{
-        increment( state ){
-            state.count++
-            state.lastMutation = 'increment'
-        },
-        incrementBy( state, value ){
-            state.count += value
-            state.lastMutation = 'incrementBy ' + value
-            state.lastRandomInt = value
-        },
-        setLoading( state, value ){
-            state.isLoading = value
-            state.lastMutation = 'setLoading ' + value
-        }
-    },
-    actions: {
-        async incrementRandom( { commit } ){
-            commit('setLoading', true)
-            const randomInt = await getRandomInt()
-
-            commit( 'incrementBy', randomInt )
-            commit('setLoading', false)
-        }
-    },
-    getters: {
-        squareCount( state ){
-            return state.count * state.count
-        }
+    modules: {
+        counter: counterStore
     }
 })
